93b1cf73f4fe3da43d833d5f816c9a3062ee353d
Hucaru <7339035+Hucaru@users.noreply.github.com>
2017-04-11 22:36:05 +0100

    Tested initial server connection with v28, need to major refactor everything

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+const (
+	protocol = "tcp"
+	address  = "0.0.0.0"
+	port     = "8484"
+)
+
+func main() {
+	fmt.Println("Test")
+
+	listener, err := net.Listen(protocol, address+":"+port)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Listener ready")
+
+	for {
+		conn, err := listener.Accept()
+
+		if err != nil {
+			fmt.Println("Error in accepting client", err)
+		}
+		fmt.Println("New Connection")
+		go handle_client_connection(conn)
+	}
+}
+
+func handle_client_connection(conn net.Conn) {
+	// Send init packet
+	conn.Write([]byte{13, 0, 28, 0, 0, 0, 28, 62, 13, 176, 236, 76, 141, 116, 8})
+	fmt.Println("Handshake sent")
+	// Send recv bullshit
+	for {
+		buffer := make([]byte, 2)
+
+		_, err := conn.Read(buffer)
+
+		if err != nil {
+			fmt.Println("Error in reading from connection")
+			return
+		}
+
+		fmt.Println("Following bytes received", buffer)
+	}
+}
+
+func run_db_query() {
+
+}
