7a036eb9170e2b74b1428a2806ab7d31916c8c4a
Hucaru <7339035+Hucaru@users.noreply.github.com>
2018-03-02 21:17:39 +0000

    More decoupling

--- maps/types.go
+++ maps/types.go
@@ -1,16 +1,6 @@
 package maps
 
-import (
-	"github.com/Hucaru/gopacket"
-)
-
 type mob interface {
-	Show() gopacket.Packet
-	Spawn() gopacket.Packet
-	UpdatePosition(int16, int16, int16)
-	RemoveController()
-	AddController()
-	IsController() bool
 }
 
 type npc interface {
