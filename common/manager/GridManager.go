b3cfc608760291930339175d3f044d9df5231836
gyqls153 <ahnb14@naver.com>
2023-01-14 05:25:55 +0900

    안정모
    쌩 맵으로 교체

--- common/manager/GridManager.go
+++ common/manager/GridManager.go
@@ -4,6 +4,7 @@ import (
 	"github.com/Hucaru/Valhalla/common"
 	"github.com/Hucaru/Valhalla/constant"
 	"github.com/Hucaru/Valhalla/mnet"
+	"github.com/sasha-s/go-deadlock"
 	"golang.org/x/exp/maps"
 )
 
@@ -13,8 +14,11 @@ type GridInfo struct {
 }
 
 type GridManager struct {
-	grids [][]ConcurrentMap[string, *mnet.Client]
-	plrs  ConcurrentMap[string, GridInfo]
+	gridLock deadlock.RWMutex
+	grids2   map[int]map[int]map[string]*mnet.Client
+	plrs2    map[string]GridInfo
+	grids    [][]ConcurrentMap[string, *mnet.Client]
+	plrs     ConcurrentMap[string, GridInfo]
 }
 
 //func (gridMgr *GridManager) Loop(f <-chan func()) {
@@ -26,8 +30,11 @@ type GridManager struct {
 //}
 
 func (gridMgr *GridManager) Init() {
+	gridMgr.gridLock = deadlock.RWMutex{}
 	gridMgr.grids = make([][]ConcurrentMap[string, *mnet.Client], 1)
 	gridMgr.plrs = New[GridInfo]()
+	gridMgr.grids2 = map[int]map[int]map[string]*mnet.Client{}
+	gridMgr.plrs2 = map[string]GridInfo{}
 
 	columns := (constant.LAND_X2 - constant.LAND_X1) / constant.LAND_VIEW_RANGE
 	rows := (constant.LAND_Y2 - constant.LAND_Y1) / constant.LAND_VIEW_RANGE
@@ -35,11 +42,13 @@ func (gridMgr *GridManager) Init() {
 	x := make([][]ConcurrentMap[string, *mnet.Client], columns)
 
 	for i := 0; i < columns; i++ {
+		gridMgr.grids2[i] = map[int]map[string]*mnet.Client{}
 		y := make([]ConcurrentMap[string, *mnet.Client], rows)
 
 		for j := 0; j < rows; j++ {
 			d := New[*mnet.Client]()
 			y[j] = d
+			gridMgr.grids2[i][j] = map[string]*mnet.Client{}
 		}
 		x[i] = y
 	}
@@ -50,22 +59,50 @@ func (gridMgr *GridManager) Init() {
 func (gridMgr *GridManager) Add(gridX, gridY int, cl *mnet.Client) {
 	plr := (*cl).GetPlayer()
 
-	gridMgr.grids[gridX][gridY].Set(plr.UId, cl)
-	gridMgr.plrs.Set(plr.UId, GridInfo{gridX, gridY})
+	gridMgr.gridLock.Lock()
+	gridMgr.grids2[gridX][gridY][plr.UId] = cl
+	gridMgr.plrs2[plr.UId] = GridInfo{gridX, gridY}
+	gridMgr.gridLock.Unlock()
+
+	//gridMgr.grids[gridX][gridY].Set(plr.UId, cl)
+	//gridMgr.plrs.Set(plr.UId, GridInfo{gridX, gridY})
 }
 
 func (gridMgr *GridManager) Remove(uId string) *mnet.Client {
-	info, ok := gridMgr.plrs.Get(uId)
-	if ok {
-		gridMgr.plrs.Remove(uId)
-		gridInfo := info
 
-		plr, ok2 := gridMgr.grids[gridInfo.GridX][gridInfo.GridY].Get(uId)
-		if ok2 {
-			gridMgr.grids[gridInfo.GridX][gridInfo.GridY].Remove(uId)
-			return plr
-		}
+	gridMgr.gridLock.RLock()
+	info, ok := gridMgr.plrs2[uId]
+	if !ok {
+		gridMgr.gridLock.RUnlock()
+		return nil
 	}
+
+	gridMgr.gridLock.RUnlock()
+
+	gridMgr.gridLock.Lock()
+	delete(gridMgr.plrs2, uId)
+
+	gridInfo := info
+	plr, ok2 := gridMgr.grids2[gridInfo.GridX][gridInfo.GridY][uId]
+	if ok2 {
+		delete(gridMgr.grids2[gridInfo.GridX][gridInfo.GridY], uId)
+		gridMgr.gridLock.Unlock()
+		return plr
+	}
+
+	gridMgr.gridLock.Unlock()
+
+	//info, ok := gridMgr.plrs.Get(uId)
+	//if ok {
+	//	gridMgr.plrs.Remove(uId)
+	//	gridInfo := info
+	//
+	//	plr, ok2 := gridMgr.grids[gridInfo.GridX][gridInfo.GridY].Get(uId)
+	//	if ok2 {
+	//		gridMgr.grids[gridInfo.GridX][gridInfo.GridY].Remove(uId)
+	//		return plr
+	//	}
+	//}
 
 	return nil
 }
@@ -96,16 +133,24 @@ func (gridMgr *GridManager) fillPlayers(GridX, GridY int) map[string]*mnet.Clien
 		GridY = MaxY - 1
 	}
 
-	itemChan := gridMgr.grids[GridX][GridY].IterBuffered()
-	for item := range itemChan {
-		result[item.Key] = item.Val
-	}
+	gridMgr.gridLock.RLock()
+	result = gridMgr.grids2[GridX][GridY]
+	gridMgr.gridLock.RUnlock()
 
 	return result
+
+	//itemChan := gridMgr.grids[GridX][GridY].IterBuffered()
+	//for item := range itemChan {
+	//	result[item.Key] = item.Val
+	//}
+	//
+	//return result
 }
 
 func (gridMgr *GridManager) OnMove(newX, newY float32, uId string) (map[string]*mnet.Client, map[string]*mnet.Client, map[string]*mnet.Client) {
-	info, ok := gridMgr.plrs.Get(uId)
+	gridMgr.gridLock.RLock()
+	info, ok := gridMgr.plrs2[uId]
+	gridMgr.gridLock.RUnlock()
 	if ok {
 		newGridX, newGridY := common.FindGrid(newX, newY)
 		gridInfo := info
@@ -113,11 +158,9 @@ func (gridMgr *GridManager) OnMove(newX, newY float32, uId string) (map[string]*
 			//gridMgr.mtx.Lock()
 			//defer gridMgr.mtx.Unlock()
 
-		} else {
-			//_plr := gridMgr.Remove(uId)
-			if true {
-				//gridMgr.Add(newGridX, newGridY, _plr)
-				gridMgr.plrs.Set(uId, GridInfo{newGridX, newGridY})
+			_plr := gridMgr.Remove(uId)
+			if _plr != nil {
+				gridMgr.Add(newGridX, newGridY, _plr)
 
 				oldList := map[string]*mnet.Client{}
 				newList := map[string]*mnet.Client{}
@@ -161,6 +204,7 @@ func (gridMgr *GridManager) OnMove(newX, newY float32, uId string) (map[string]*
 
 				return addList, removeList, newList
 			}
+		} else {
 			newList := map[string]*mnet.Client{}
 			for i := -1; i <= 1; i++ {
 				for j := -1; j <= 1; j++ {
@@ -178,4 +222,77 @@ func (gridMgr *GridManager) OnMove(newX, newY float32, uId string) (map[string]*
 	}
 
 	return nil, nil, nil
+
+	//info, ok := gridMgr.plrs.Get(uId)
+	//if ok {
+	//	newGridX, newGridY := common.FindGrid(newX, newY)
+	//	gridInfo := info
+	//	if newGridX != gridInfo.GridX || newGridY != gridInfo.GridY {
+	//		//gridMgr.mtx.Lock()
+	//		//defer gridMgr.mtx.Unlock()
+	//
+	//		_plr := gridMgr.Remove(uId)
+	//		if _plr != nil {
+	//			gridMgr.Add(newGridX, newGridY, _plr)
+	//
+	//			oldList := map[string]*mnet.Client{}
+	//			newList := map[string]*mnet.Client{}
+	//
+	//			for i := -1; i <= 1; i++ {
+	//				for j := -1; j <= 1; j++ {
+	//					oldGridX := gridInfo.GridX + i
+	//					oldGridY := gridInfo.GridY + j
+	//
+	//					_newGridX := newGridX + i
+	//					_newGridY := newGridY + j
+	//
+	//					maps.Copy(oldList, gridMgr.fillPlayers(oldGridX, oldGridY))
+	//					maps.Copy(newList, gridMgr.fillPlayers(_newGridX, _newGridY))
+	//				}
+	//			}
+	//
+	//			delete(oldList, uId)
+	//			delete(newList, uId)
+	//
+	//			removeList := map[string]*mnet.Client{}
+	//			addList := map[string]*mnet.Client{}
+	//
+	//			for k, v := range oldList {
+	//				_, ok := newList[k]
+	//				if ok {
+	//					continue
+	//				}
+	//
+	//				removeList[k] = v
+	//			}
+	//
+	//			for k, v := range newList {
+	//				_, ok := oldList[k]
+	//				if ok {
+	//					continue
+	//				}
+	//
+	//				addList[k] = v
+	//			}
+	//
+	//			return addList, removeList, newList
+	//		}
+	//	} else {
+	//		newList := map[string]*mnet.Client{}
+	//		for i := -1; i <= 1; i++ {
+	//			for j := -1; j <= 1; j++ {
+	//				_newGridX := newGridX + i
+	//				_newGridY := newGridY + j
+	//
+	//				maps.Copy(newList, gridMgr.fillPlayers(_newGridX, _newGridY))
+	//			}
+	//		}
+	//
+	//		delete(newList, uId)
+	//
+	//		return nil, nil, newList
+	//	}
+	//}
+	//
+	//return nil, nil, nil
 }
