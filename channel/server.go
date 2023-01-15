8defd9e3e2c9d885316da918b6f75aaeacd3747a
gyqls153 <ahnb14@naver.com>
2023-01-15 16:54:40 +0900

    Update server.go

--- channel/server.go
+++ channel/server.go
@@ -235,9 +235,20 @@ func (server *Server) Initialize(work chan func(), dbuser, dbpassword, dbaddress
 	go func() {
 		for {
 			log.Println(runtime.NumCPU(), runtime.NumGoroutine(), runtime.NumCgoCall())
+			var m runtime.MemStats
+			runtime.ReadMemStats(&m)
+			// For info on each, see: https://golang.org/pkg/runtime/#MemStats
+			fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
+			fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
+			fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
+			fmt.Printf("\tNumGC = %v\n", m.NumGC)
 			time.Sleep(1000 * time.Millisecond)
 		}
 	}()
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
 }
 
 func (server *Server) addToEmulateMoving(uid string, plrs []*player) {
