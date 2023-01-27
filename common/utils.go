39833e9664b46f8ebdd090d10ef83539e8eef0b2
gyqls153 <ahnb14@naver.com>
2023-01-27 11:37:07 +0900

    안정모
    그리드 예외 처리 메모리 폭증으로 꼼꼼하게 예외처리하고 사이즈 줄임

--- common/utils.go
+++ common/utils.go
@@ -5,6 +5,9 @@ import (
 )
 
 func FindGrid(mX float32, mY float32) (int, int) {
+	MaxX := (constant.LAND_X2 - constant.LAND_X1) / constant.LAND_VIEW_RANGE
+	MaxY := (constant.LAND_Y2 - constant.LAND_Y1) / constant.LAND_VIEW_RANGE
+
 	if mX > constant.LAND_X2 {
 		mX = constant.LAND_X2
 	} else if mX < constant.LAND_X1 {
@@ -17,8 +20,24 @@ func FindGrid(mX float32, mY float32) (int, int) {
 		mY = constant.LAND_Y1
 	}
 
-	x := (mX - constant.LAND_X1) / constant.LAND_VIEW_RANGE
-	y := (mY - constant.LAND_Y1) / constant.LAND_VIEW_RANGE
+	x := int((mX - constant.LAND_X1) / constant.LAND_VIEW_RANGE)
+	y := int((mY - constant.LAND_Y1) / constant.LAND_VIEW_RANGE)
+
+	if 0 > x {
+		x = 0
+	}
+
+	if 0 > y {
+		y = 0
+	}
+
+	if x > MaxX-1 {
+		x = MaxX - 1
+	}
+
+	if y > MaxY-1 {
+		y = MaxY - 1
+	}
 
 	return int(x), int(y)
 }
