9f5d8495c961e0d71caf1b04c0e13b3c9097f3b9
Erwin Evans <zew732@my.utsa.edu>
2025-09-17 10:27:55 -0700

    fix cabs

--- scripts/npc/1052012.js
+++ scripts/npc/1052012.js
@@ -1,3 +1,21 @@
-```javascript
-npc.sendOk("Hey, hey ... I don't think you're logging on from the internet cafe. You can't enter this place if you are logging on from home ...")
-```
+// Internet Cafe Access (Paid Entry)
+var COST = 1000000;
+
+npc.sendBackNext(
+    "Hey there. Normally, only users logged in from an Internet Cafe can access the Premium Road.\r\n" +
+    "If you're not at a cafe, I can still get you in... for a price.", false, true
+);
+
+if (npc.sendYesNo(
+    "Want access to the Internet Cafe Premium Road right now for #b1,000,000 mesos#k?\r\n" +
+    "Inside you'll find boosted EXP, drops, and mesos. Shall I send you?"
+)) {
+    if (plr.mesos() < COST) {
+        npc.sendOk("You don't have enough mesos. Come back when you've got #b1,000,000#k.");
+    } else {
+        plr.giveMesos(-COST);
+        plr.warp(193000000);
+    }
+} else {
+    npc.sendBackNext("No problem. If you change your mind, I can grant access anytime for #b1,000,000 mesos#k.", false, true);
+}
--- scripts/npc/1052013.js
+++ scripts/npc/1052013.js
@@ -1,10 +1,22 @@
 // Internet-Cafe Premium Road warper
-npc.sendNext("Bzzzt~Beep~Boop!! Welcome.. to the Internet Cafe!!.. I.. can warp.. you.. to different.. training areas.. within.. the special.. Internet Cafe.. Premium Road.. areas.... Experience points.. from.. monsters.. are doubled.. as well as.. drops.. and.. mesos!!..")
 
-var sel = npc.sendMenu("Please.. choose.. a.. destination....",
-                       "#m190000000#",
-                       "#m191000000#",
-                       "#m192000000#",
-                       "#m195000000#")
+var maps = [190000000, 191000000, 192000000, 195000000];
 
-plr.warp([190000000, 191000000, 192000000, 195000000][sel])
+npc.sendBackNext(
+    "Bzzzt~Beep~Boop!! Welcome.. to the Internet Cafe!!.. I.. can warp.. you.. to different.. training areas.. within.. the special.. Internet Cafe.. Premium Road.. areas.... Experience points.. from.. monsters.. are doubled.. as well as.. drops.. and.. mesos!!..",
+    false, true
+);
+
+var text = "Please.. choose.. a.. destination....\r\n";
+for (var i = 0; i < maps.length; i++) {
+    text += "#L" + i + "##m" + maps[i] + "##l\r\n";
+}
+
+npc.sendSelection(text);
+
+var sel = npc.selection();
+if (sel < 0 || sel >= maps.length) {
+    npc.sendOk("Bzzzt.. invalid.. selection..");
+} else {
+    plr.warp(maps[sel]);
+}
