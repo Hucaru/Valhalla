df2e7e448e60e2da9553b66af6051b331998312c
Erwin Evans <zew732@my.utsa.edu>
2025-11-13 10:14:07 -0800

    fix some npcs

--- scripts/npc/1002002.js
+++ scripts/npc/1002002.js
@@ -1,6 +1,6 @@
 // Pason – White Wave Harbor (120020400) – stateless
 
-npc.sendNext("You can take the boat, if you like. Where would you like to go?\r\n\r\n#L0#Florina Beach, please.#l", false, true)
+npc.sendSelection("You can take the boat, if you like. Where would you like to go?\r\n\r\n#L0#Florina Beach, please.#l")
 
 var sel = npc.selection()
 if (sel == 0) {
@@ -13,23 +13,27 @@ if (sel == 0) {
     if (sel == 0) {                                            // Pay 2000 Mesos
         if (npc.sendYesNo("You want to pay #b2000 Mesos#k to go to #m120030000#? Sure, but don't forget that there are monsters there, too. I'll prepare to set sail. Okay. You ready to head for #m120030000# right now?")) {
             if (plr.mesos() < 2000) {
-                npc.sendNext("I think you're lacking mesos. There are many ways to gather up some money, you know, like... selling your armor... defeating monsters... doing quests... you know what I'm talking about.")
+                npc.sendBackNext("I think you're lacking mesos. There are many ways to gather up some money, you know, like... selling your armor... defeating monsters... doing quests... you know what I'm talking about.", false, true)
             } else {
                 plr.giveMesos(-2000)
-                plr.warp(120030000)
+                plr.warp(110000000)
             }
         } else {
-            npc.sendNext("You must have some business to take care of here. You must be tired from all that traveling and hunting. Go take some rest, and if you feel like changing your mind, then come talk to me.")
+            npc.sendBackNext("You must have some business to take care of here. You must be tired from all that traveling and hunting. Go take some rest, and if you feel like changing your mind, then come talk to me.", false, true)
         }
     } else if (sel == 1) {                                      // Use VIP Ticket
         if (npc.sendYesNo("So you have a #bVIP Ticket to Florina Beach#k? You can always head over to Florina Beach with that. Alright then, but just be aware that you may be running into some monsters there too. Okay, would you like to head over to Florina Beach right now?")) {
             if (plr.itemCount(4031134) < 1) {
-                npc.sendNext("Hmmm, so where exactly is your #bVIP Ticket to Florina Beach#k? Are you sure you have one? Please double-check.")
+                npc.sendBackNext("Hmmm, so where exactly is your #bVIP Ticket to Florina Beach#k? Are you sure you have one? Please double-check.", false, true)
             } else {
-                plr.warp(120030000)
+                if (!plr.removeItemsByID(4031134, 1)) {
+                    npc.sendBackNext("Something went wrong while taking your #bVIP Ticket to Florina Beach#k. Please try again.", false, true)
+                } else {
+                    plr.warp(110000000)
+                }
             }
         } else {
-            npc.sendNext("You must have some business to take care of here. You must be tired from all that traveling and hunting. Go take some rest, and if you feel like changing your mind, then come talk to me.")
+            npc.sendBackNext("You must have some business to take care of here. You must be tired from all that traveling and hunting. Go take some rest, and if you feel like changing your mind, then come talk to me.", false, true)
         }
     } else if (sel == 2) {                                      // Explain VIP ticket
         npc.sendBackNext("You must be curious about a #bVIP Ticket to Florina Beach#k. Haha, that's very understandable. A VIP Ticket to Florina Beach is an item where as long as you have in possession, you may make your way to Florina Beach for free. It's such a rare item that even we had to buy those, but unfortunately I lost mine a few weeks ago during my precious summer break.", false, true)
