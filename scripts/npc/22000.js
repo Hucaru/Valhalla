d657736b9ac58bd1e1916f64365d98653e0f7d56
Erwin <44278151+ErwinsExpertise@users.noreply.github.com>
2026-01-14 11:50:26 -0800

    taxi discount and lith fix (#238)

--- scripts/npc/22000.js
+++ scripts/npc/22000.js
@@ -5,7 +5,7 @@ if (plr.itemCount(4031801) > 0) {
         npc.sendBackNext("Okay, now give me 150 mesos...Hey, what's that? Is that the recommendation letter from Lucas, the chief of Amherst? You should have told me about this earlier. I, Shanks, recognize greatness when l see it, and since you have been recommended by Lucas, l can see that you have very great potential as an adventurer. No way would l dare charge you for this trip!")
         npc.sendBackNext("Since you have the recommendation letter, I won't charge you for this. We're going to head to Victoria Island right now, so buckle up! it might get a bit turbulent!!")
         plr.removeItemsByID(4031801, 1)
-        plr.warp(104000000)
+        plr.warpToPortalName(104000000, "maple00")
     } else {
         npc.sendOk("Hmm... I guess you still have things to do here?");
     }
